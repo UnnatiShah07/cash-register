bc6c2831ce978bcb4bafb8be5c1fd3ff53c9515c
UnnatiShah07 <76685572+UnnatiShah07@users.noreply.github.com>
2022-09-27 22:57:59 +0530

    Complete cash register app

---
+++ index.js
@@ -0,0 +1,43 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkBtn = document.querySelector("#check-btn");
+const errorMsg = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll("#no-of-notes");
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkBtn.addEventListener("click", validateAmount);
+
+function validateAmount() {
+    hideError();
+
+    if (billAmount.value > 0) {
+        if (Number(cashGiven.value) >= Number(billAmount.value)) {
+            const amountToBeReturn = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturn);
+        } else {
+            showError(
+                "The cash provided should atleast be equal to the bill amount."
+            );
+        }
+    } else {
+        showError("Invalid Bill Amount.");
+    }
+}
+
+function hideError() {
+    errorMsg.style.display = "none";
+}
+
+function showError(message) {
+    errorMsg.style.display = "block";
+    errorMsg.innerHTML = message;
+}
+
+function calculateChange(amountToBeReturn) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(amountToBeReturn / availableNotes[i]);
+        amountToBeReturn %= availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
